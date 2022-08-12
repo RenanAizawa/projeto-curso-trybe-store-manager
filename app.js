5f4fff682f667d1bdcf356d4c7aae4329ec41ac2
Renan Aizawa Alves Mascarenhas <renanaizawa@outlook.com>
2022-08-12 16:46:22 -0400

    feat: iniciando estatus

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ app.get('/', (_request, response) => {
   response.send();
 });
 
+app.get('/products', controler.getAllProd());
+
+app.get('/prod/:id', controler.getProdId());
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
 // você deve usar o arquivo index.js para executar sua aplicação 
