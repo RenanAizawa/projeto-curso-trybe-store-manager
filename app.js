d7edf88e288906066581ecc3b24f0ccd0f50c716
Renan Aizawa Alves Mascarenhas <renanaizawa@outlook.com>
2022-08-12 21:58:44 -0400

    atualização: criação do connection

--- app.js
+++ app.js
@@ -7,9 +7,6 @@ app.get('/', (_request, response) => {
   response.send();
 });
 
-app.get('/products', controler.getAllProd());
-
-app.get('/prod/:id', controler.getProdId());
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
 // você deve usar o arquivo index.js para executar sua aplicação 
